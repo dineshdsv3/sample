85940b5c709becabcd832b7b63c3f65000b44bae
dineshdsv3 <43554866+dineshdsv3@users.noreply.github.com>
2019-06-02 01:46:24 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,6 @@
+function addition() {
+   
+   let a =  +(document.getElementById("firstNum").value);
+   let b =  +(document.getElementById("secondNum").value);
+    document.dinesh.hari.value =  (a +b);
+}
